53a534633e56bb383a5fbcfa6da12a1250f19bde
Bibi Mathew <mbibi95@gmail.com>
2025-01-30 20:25:43 -0500

    changing console.log

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -6,6 +6,10 @@ axios.defaults.baseURL = import.meta.env.DEV
   ? "http://localhost:8080"
   : "https://music-app-api-oq6b.onrender.com";
 
+console.log(" Testing Axios Base URL:", axios.defaults.baseURL);
+// axios.defaults.baseURL =
+//   import.meta.env.VITE_API_URL || "http://localhost:8080";
+
 // axios.defaults.baseURL = "https://music-app-api-oq6b.onrender.com";
 
 export default axios;
