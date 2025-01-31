77be0b100852bf3bc6df813e2d140f31bae50800
Bibi Mathew <mbibi95@gmail.com>
2025-01-30 21:58:41 -0500

    attempt

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 console.log(" Testing Axios Base URL:", axios.defaults.baseURL);
 
-axios.defaults.baseURL = import.meta.env.DEV
-  ? "http://localhost:8080"
-  : "https://music-app-api-oq6b.onrender.com";
+// axios.defaults.baseURL = import.meta.env.DEV
+//   ? "http://localhost:8080"
+//   : "https://music-app-api-oq6b.onrender.com";
+
+axios.defaults.baseURL = "https://music-app-api-oq6b.onrender.com";
 
 console.log(" Testing Axios Base URL:", axios.defaults.baseURL);
 // axios.defaults.baseURL =
