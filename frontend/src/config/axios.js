ec432d0de2cbc0f102b3f629114e1d825ca79c6b
Bibi Mathew <mbibi95@gmail.com>
2025-01-30 20:17:06 -0500

    test

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("Axios Base URL:", axios.defaults.baseURL);
+
 axios.defaults.baseURL = import.meta.env.DEV
   ? "http://localhost:8080"
   : "https://music-app-api-oq6b.onrender.com";
